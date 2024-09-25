e73182b2f580766f5b675b84ca41eabae187d44e
josh-hsiehh <joshua.hsieh@sjsu.edu>
2024-09-24 18:22:37 -0700

    commit

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,68 +2,58 @@
 //plan is to add a lot of images of us
 
 const About = function About() {
-    return (
-        <div className="mt-4 font-light text-center">
-            <div className="font-bold">
-                WHO WE ARE ... add some images like a carousel?
-            </div>
+  return (
+    <div className="mt-4 text-center font-light">
+      <div className="font-bold">WHO WE ARE ... add some images like a carousel?</div>
 
-            <div>
-                <br />
-                Founded in 1904 at the University of Minnesota in Minneapolis as the Society of Hammer and Tongs by four engineering students, Theta Tau is the oldest, largest, and foremost Fraternity for Engineers. Today, Theta Tau proudly has a diverse membership of men and women studying engineering and computer science at more than 80 campuses across the US.
-            </div>
-            <div>
-                <br />
-                <div className="font-bold">
-                    OMEGA EPSILON CHAPTER HISTORY , add like a timeline
-                </div>
+      <div>
+        <br />
+        Founded in 1904 at the University of Minnesota in Minneapolis as the Society of Hammer and Tongs by four
+        engineering students, Theta Tau is the oldest, largest, and foremost Fraternity for Engineers. Today, Theta Tau
+        proudly has a diverse membership of men and women studying engineering and computer science at more than 80
+        campuses across the US.
+      </div>
+      <div>
+        <br />
+        <div className="font-bold">OMEGA EPSILON CHAPTER HISTORY , add like a timeline</div>
+        <br />
+        <br />
+        In Spring of 2018, Mark Muendelein met a handful of brothers from Epsilon Delta Chapter of Theta Tau, chartered
+        at University of California, San Diego. It was the same Spring he met the Theta Tau brothers that he finished
+        his term as the Vice President of Alumni Relations with Alpha Kappa Psi. By the end of the semester, he had
+        realized that his community and his career were destined for two different directions. In an attempt to bridge
+        this gap he saw, Muendelein decided to enlist friends, peers, colleagues, and mentors to begin a chapter of
+        Theta Tau at San Jose State University.
+      </div>
 
-                <br />
-                <br />
-                In Spring of 2018, Mark Muendelein met a handful of brothers from Epsilon Delta Chapter of Theta
-                Tau, chartered at University of California, San Diego. It was the same Spring he met the Theta Tau brothers
-                that he finished his term as the Vice President of Alumni Relations with Alpha Kappa Psi. By the end of the
-                semester, he had realized that his community and his career were destined for two different directions. In
-                an attempt to bridge this gap he saw, Muendelein decided to enlist friends, peers, colleagues, and mentors
-                to begin a chapter of Theta Tau at San Jose State University.
-            </div>
-
-            <div>
-                <br />
-                <div className = "font-bold">
-                    NATIONAL HISTORY
-                </div>
-                
-                
-                <br />
-                Omega Epsilon is part of the Western Region of Theta Tau.
-                <br />
-                <br />
-
-                <ul class="list-disc list-inside ml-4 text-white-800">
-                    <li>Chi Chapter (University of Arizona)</li>
-                    <li>Delta Gamma Chapter (Arizona State University)</li>
-                    <li>Epsilon Chapter (University of California, Berkeley)</li>
-                    <li>Epsilon Delta Chapter (University of California, San Diego)</li>
-                    <li>Kappa Epsilon Chapter (University of Southern California)</li>
-                    <li>Lambda Delta Chapter (University of the Pacific)</li>
-                    <li>Lambda Epsilon Chapter (University of San Diego)</li>
-                    <li>Mu Delta Chapter (University of California, Merced)</li>
-                    <li>Omicron Epsilon Chapter (Northern Arizona University)</li>
-                    <li>Omicron Gamma Chapter (University of California, Davis)</li>
-                    <li>Phi Epsilon (California State University, Fullerton)</li>
-                    <li>Rho Delta Chapter (University of Nevada, Reno)</li>
-                    <li>Sigma Delta Chapter (University of California, Riverside)</li>
-                    <li>Sigma Epsilon Chapter (University of California, Santa Barbara)</li>
-                    <li>Upsilon Delta Chapter (University of California, Los Angeles)</li>
-                    <li>Upsilon Epsilon (Santa Clara University)</li>
-                    <li>Xi Epsilon Chapter (California State University, Long Beach)</li>
-                </ul>
-
-            </div>
-
-        </div>
-    );
+      <div>
+        <br />
+        <div className="font-bold">NATIONAL HISTORY</div>
+        <br />
+        Omega Epsilon is part of the Western Region of Theta Tau.
+        <br />
+        <br />
+        <ul class="text-white-800 ml-4 list-inside list-disc">
+          <li>Chi Chapter (University of Arizona)</li>
+          <li>Delta Gamma Chapter (Arizona State University)</li>
+          <li>Epsilon Chapter (University of California, Berkeley)</li>
+          <li>Epsilon Delta Chapter (University of California, San Diego)</li>
+          <li>Kappa Epsilon Chapter (University of Southern California)</li>
+          <li>Lambda Delta Chapter (University of the Pacific)</li>
+          <li>Lambda Epsilon Chapter (University of San Diego)</li>
+          <li>Mu Delta Chapter (University of California, Merced)</li>
+          <li>Omicron Epsilon Chapter (Northern Arizona University)</li>
+          <li>Omicron Gamma Chapter (University of California, Davis)</li>
+          <li>Phi Epsilon (California State University, Fullerton)</li>
+          <li>Rho Delta Chapter (University of Nevada, Reno)</li>
+          <li>Sigma Delta Chapter (University of California, Riverside)</li>
+          <li>Sigma Epsilon Chapter (University of California, Santa Barbara)</li>
+          <li>Upsilon Delta Chapter (University of California, Los Angeles)</li>
+          <li>Upsilon Epsilon (Santa Clara University)</li>
+          <li>Xi Epsilon Chapter (California State University, Long Beach)</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default About;
-
